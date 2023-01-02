9763cae5b6f4037a4c2890a7e3cdcbbaba11876c
Mary <itsmaryyang@gmail.com>
2023-01-01 21:36:55 -0600

    Add multiple choice select code to JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-var answerInput = document.querySelector("#multipleAnswers");
 var scoreCount = document.querySelector("#score");
 var timerTiktok = document.querySelector("#time");
 var submitButton = document.querySelector("#submit-next-btn");
 var startButton = document.querySelector("#start-button");
 var startQuiz = document.querySelector("#start-page");
 var questionsQuiz = document.querySelector("#questions");
+var multipleChoice = document.querySelector("#mult-choice");
 
 var count = 0;
 
-var totalScore = [];
+var count = localStorage.getItem("count");
+
+scoreCount.textContent = count;
 
 var questionOne = "Arrays can store what type of values?"
 var questionTwo = "The condition of an if/else statement is enclosed with?"
@@ -16,22 +18,21 @@ var questionThree = "The script.js link should be located where?"
 var questionFour = "Can arrays be mutated?"
 var questionFive = "What are common methods for array functions?"
 
-
-
 // The following code is to start the quiz
-
 startButton.addEventListener("click", function() {
   startQuiz.classList.add('hide');
   startButton.classList.add('hide');
   questionsQuiz.classList.remove('hide');
   timerTiktok.classList.remove('hide');
+  submitButton.classList.remove('hide');
+  multipleChoice.classList.remove('hide');
 setTime();
 firstQuestion();
 });
 
 
 // The following code is to set the timer for the quiz
-var secondsRemaining = 10;
+var secondsRemaining = 60;
 
 function setTime() {
   var timerInterval = setInterval(function() {
@@ -45,21 +46,6 @@ function setTime() {
   }, 1200);
 };
 
-// The following code is to display next question
-function firstQuestion(()=> {
-
-}
-);
-
-// The following code is to select answer
-
-
-// submitButton.addEventListener("click", );
-/*
-
-
-// The following code is to create a list of multiple choice answers for 5 questions
-// Multiple Choice answers 1
 var listEl = document.createElement("ol");
 
 var li1 = document.createElement("li");
@@ -67,18 +53,55 @@ var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 var li4 = document.createElement("li");
 
-answerInput.appendChild(listEl);
+// The following code is to display first question and multiple choice answers
+function firstQuestion () {
+ questionsQuiz.append(questionOne);
+ multipleChoice.appendChild(listEl);
 
-listEl.appendChild(li1);
-listEl.appendChild(li2);
-listEl.appendChild(li3);
-listEl.appendChild(li4);
+
+listEl.appendChild(li1).addEventListener("click", function(){
+  li1.setAttribute("style", "font-size: 25px; font-weight: bold; text-decoration: underline;")
+  console.log("clicked choice 1");
+
+});
+listEl.appendChild(li2).addEventListener("click", function (){
+  li2.setAttribute("style", "font-size: 25px; font-weight: bold; text-decoration: underline;")
+ console.log("clicked choice 2");
+});
+listEl.appendChild(li3).addEventListener("click", function (){
+  li3.setAttribute("style", "font-size: 25px; font-weight: bold; text-decoration: underline;")
+  console.log("clicked choice 3");
+});
+listEl.appendChild(li4).addEventListener("click", function (){
+  li4.setAttribute("style", "font-size: 25px; font-weight: bold; text-decoration: underline;")
+  console.log("clicked choice 4");
+  }
+);
 
 li1.textContent = "Booleans";
-li2.textContent = "numbers";
-li3.textContent = "strings";
+li2.textContent = "Numbers";
+li3.textContent = "Strings";
 li4.textContent = "All the Above";
 
+};
+
+
+// the following code is to reset state and start next question and multiple choice
+
+/*
+function resetQuiz () {
+  submitButton.classList.add('hide');
+  console.log("delete button worked");
+}
+
+// The following code is to select answer
+submitButton.addEventListener("click", function(event) {
+  var element = event.target;
+  console.log("event");
+});
+*/
+
+/*
 // Multiple Choice answers 2
 
 answerInput.appendChild(listEl);
@@ -140,7 +163,7 @@ li4.textContent = "splice()";
 
 //code is to assign points to correct answers
 if(answerInput === li4) {
-  console.log(scoreCount === 5);
+  console.log(scoreCount === 1);
 } else {
   console.log(scoreCount === 0);
 }
